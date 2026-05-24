11176cbe608ee543c108490046dffd611dd54923
rsathish <rasthish@1526@gmail.com>
2026-05-24 23:23:58 +0530

    updated javascript programs

--- j script.js
+++ j script.js
@@ -1,214 +1,753 @@
+Task:1
 
-Task:2
+// // 1. Create a variable using var and store your name.
+// var name = "sathish"
 
-// 1. Create a variable and store your favorite food.
+// // 2. Print your name using console.log().
+// console.log(name)
 
-let f = "favorite food"
-console.log(f)
+// // 3. Create a variable using let and store your age.
+// let age = 26
 
-// 2. Create a variable and store your mobile number.
+// // 4. Change the value of age and print it.
+// age = 27
+// console.log(age)
 
-let n = 8870054120
-console.log(n)
+// // 5. Create a constant variable for country name.
+// const cname = "india"
 
-// 3. Create a boolean variable with false value.
-
-let bad = "false"
-
-// 4. Create one undefined variable and print typeof.
-
-let cod = ""
-console.log(typeof cod)
-
-// 5. Create one array with 6 animal names.
-
-let animals =["Dog","Cat","Deer","Rabbit","Lion","Tiger"]
-console.log(animals)
-
-// 6. Print second animal from array.
-
-console.log(animals[1])
-
-// 7. Print last animal from array using length.
-
-console.log(animals[animals.length-1])
+// // 6. Print the country name.
+// console.log(cname)
 
 
-// 8. Create object with bike details (name, model, color).
+// // 7. Try redeclaring var variable and observe output.
 
-let bike = {
-name:"honda", 
-model:"cbr",
-color:"black"
+// var d = 20
+//     d = 22
+// var d = 23
+// console.log(d)   
+
+// // 8. Try redeclaring let variable and observe error.
+
+// let e = 44
+//     e = 45
+// let e = 46
+// console.log(e)//sysntax error
+
+// // 9. Try reassigning const variable and observe error.
+
+// const seconds = 10
+//       seconds = 20
+// const seconds = 30
+// console.log(seconds) // Error:identifier has been already declared     
+
+// //10. Write difference between var and let.
+
+// var:it can be declared ,initialize ,reuse ,redeclare and reinitialized.
+// let:it can be declared ,initialize ,reuse and reinitialized only it cannot be redeclared.
+
+// //11. Write difference between let and const.
+
+// let:it can be declared ,initialize ,reuse and reinitialized only it cannot be redeclared.
+// constant:it can be declared and initialize once only it cannot be reused ,redeclared or reinitialized.
+
+// // 12. Predict output
+
+// var a = 10
+// a = 20
+// var a = 30
+
+// console.log(a)// OUTPUT:30
+
+// // 13. Predict output
+
+// let b = 50
+// b = 60
+
+// console.log(b)// OUTPUT:60
+
+
+// // 14. Predict output
+
+// const c = 100
+
+// console.log(c)// OUTPUT:100
+
+
+// // 15. Use console.log() to print "Hello World".
+// console.log("Hello world")
+
+// // 16. Use alert() to show "Welcome User".
+// let message = "Welcome user"
+// alert(message)
+
+// // 17. Use confirm() to ask user:
+// // "Do you like JavaScript?"
+// confirm("Do you like JavaScript?")
+
+// // 18. Use prompt() to ask user name.
+// prompt("User Name")
+
+// // 19. Print prompt value in console.
+// let n = prompt(25)
+// console.log(prompt)
+
+
+// // 20. Use document.writeln() to print "Batch 33".
+// document.writeln("Batch 33")
+
+// // 21. Create variable studentName and store your name.
+// var studentName="sathish"
+// console.log(studentName)
+
+// // 22. Create variable studentAge and store your age.
+// var studentAge = 26 
+// console.log(studentAge)
+
+// // 23. Print both variables together.
+// var studentName="sathish"
+// var studentAge = 26
+// console.log(studentName )
+// console.log(studentAge)
+
+// // 24. Create a variable for mobile number.
+// let mobilenumber = 8870054120
+
+// // 25. Print mobile number using console.log().
+// console.log(8870054120)
+
+// // 26. Create variable using var for favorite color.
+// var favoritecolor = "black"
+
+// // 27. Reassign another color value.
+// var favoritecolor = 50
+
+// // 28. Create variable using let for marks.
+// let marks = 98
+
+// // 29. Change marks value and print it.
+// marks = 99
+// console.log(marks)
+
+// // 30. Create const variable for PI value.
+// const pivalue = 97
+
+// // 31. Print PI value.
+// console.log(pivalue)
+
+// // 32. Predict output
+
+// var x = 5
+// var x = 15
+
+// console.log(x) //OUTPUT:15
+
+// // 33. Predict output
+
+// let y = 25
+// y = 35
+
+// console.log(y) //OUTPUT:35
+
+
+// // 34. Find error
+
+// const z = 40
+// z = 45
+
+// console.log(z)//Error:we canot reuse redeclare or reinitialize it is fixed one 
+
+// //35. Create alert box with your college name.
+// alert("Kongu arts and science college")
+
+// //36. Ask user age using prompt().
+// prompt("Enter Your Age")
+
+// //37. Print user age in console.
+// console.log("Enter Your Age")
+
+// //38. Ask user confirmation:
+// "Did you complete HTML?"
+// confirm("Did you complete HTML?")
+
+// //39. Print confirm result in console.
+// if(true)
+// {
+// console.log("completed");
+
+// }
+// else
+// {
+// console.log("not completed");
+// }
+
+
+// //40. Print "JavaScript Class" using document.writeln().
+// document.writeln("JavaScript Class")
+
+
+// //41. Create 3 variables:
+// city, state, country
+// var city="tirupur"
+// var state="tamilnadu"
+// var country="india"
+
+
+// //42. Print all variables together.
+// console.log(city+" "+state+ " "+country+"  ")
+
+// //43. Create one example for var.
+// var base="js"
+//    base="javascript"
+// var base="jscript"
+// console.log(base)
+
+// //44. Create one example for let.
+// let o=10
+//    j=12
+// let u=33
+// console.log(o+u)
+
+// //45. Create one example for const.
+// const z=70
+//      x=40
+// console.log(z)
+
+// //46. Predict output
+
+// var num = 100
+// num = 200
+
+// console.log(num) //OUTPUT = 200
+
+
+// // 47. Predict output
+
+// let price = 500
+// price = 700
+
+// console.log(price) //OUTPUT = 700
+
+
+// // 48. Predict output
+
+// const password = 12345
+
+// console.log(password) :OUTPUT = 12345
+
+
+// //49. Write rules of var.
+//     var can be redeclared
+//     var can be reassigned
+//     var can declared without value
+//     var can be changes anywhere 
+//     var has a function scope
+
+// //50. Write rules of let and const.
+//     let can be reassigned
+//     let cannot be redeclared
+//     let cannot declared without value
+//     let is a block scope
+
+//     const is fixed one
+//     const cannot be redeclared
+//     const cannot be reassigned
+//     const is block
+
+// Task:2
+
+// // 1. Create a variable and store your favorite food.
+
+// let f = "favorite food"
+// console.log(f)
+
+// // 2. Create a variable and store your mobile number.
+
+// let n = 8870054120
+// console.log(n)
+
+// // 3. Create a boolean variable with false value.
+
+// let bad = "false"
+
+// // 4. Create one undefined variable and print typeof.
+
+// let cod = ""
+// console.log(typeof cod)
+
+// // 5. Create one array with 6 animal names.
+
+// let animals =["Dog","Cat","Deer","Rabbit","Lion","Tiger"]
+// console.log(animals)
+
+// // 6. Print second animal from array.
+
+// console.log(animals[1])
+
+// // 7. Print last animal from array using length.
+
+// console.log(animals[animals.length-1])
+
+
+// // 8. Create object with bike details (name, model, color).
+
+// let bike = {
+// name:"honda", 
+// model:"cbr",
+// color:"black"
+// }
+// console.log(bike)
+
+// // 9. Print bike color from object.
+
+// console.log(bike.color)
+
+// // 10. Create two variables and subtract values.
+
+// let q=10
+//     w=5
+// let sub=q-w
+// console.log(sub)
+
+// // 11. Create two variables and divide values.
+
+// let r=10
+//     t=2
+// let div=r/t
+// console.log(div)
+
+// // 12. Find 15 % 2.
+// let find=15%2
+// console.log(find)
+// OUTPUT:1
+
+// // 13. Find 2 ** 5.
+
+// let fin=2**5
+// console.log(fin)
+// OUTPUT:32
+
+// // 14. Create variable with 5 and use post increment.
+
+// let a=5;
+//     a++
+// console.log(a)
+
+// // 15. Create variable with 10 and use pre increment.
+
+// let no=10;
+//     ++no;
+// console.log(no)
+
+// // 16. Create variable with 20 and use post decrement.
+
+// let de=11;
+//     de--;
+// console.log(de)
+
+// // 17. Create variable with 50 and use pre decrement.
+
+// let va=20;
+//     --va;
+// console.log(va)
+
+// // 18. Check 100 > 50.
+
+// console.log(100>50) //true
+
+// // 19. Check 25 < 10.
+
+// console.log(25 < 10)//false
+
+// // 20. Check 50 >= 50.
+
+// console.log(50 >= 50)//true
+
+// // 21. Check 30 <= 25.
+
+// console.log(30 <= 25)//false
+
+// // 22. Check 10 == "10".
+
+// console.log(10 == "10")//true
+
+// // 23. Check 10 === "10".
+
+// console.log(10 === "10")//false
+
+// // 24. Check 25 != "25".
+
+// console.log(25 != "25")//false
+
+// // 25. Check 25 !== "25".
+
+// console.log(25 !== "25")//true
+
+// // 26. Use AND operator:
+// //     10 > 5 && 20 > 15
+
+// console.log(10 > 5 && 20 > 15)//true
+
+// // 27. Use AND operator:
+// //     5 > 10 && 20 > 15
+
+// console.log(5 > 10 && 20 > 15)//false
+
+// // 28. Use OR operator:
+// //     5 > 10 || 20 > 15
+
+// console.log(5 > 10 || 20 > 15)//true
+
+// // 29. Use OR operator:
+// //     2 > 5 || 1 > 10
+
+// console.log(2 > 5 || 1 > 10)//false
+
+// // 30. Use NOT operator:
+// //     !(10 > 5)
+
+// console.log(!(10 > 5))//false
+
+// // 31. Use NOT operator:
+// //     !(5 > 10)
+
+// console.log(!(5 > 10))//true
+
+// // 32. Create ternary operator:
+// //     if 50 > 25 print "True"
+// //     else print "False"
+
+// let l = 50 > 25;
+
+// if(l){console.log(true);}
+// else{console.log(false);}
+// // 33. Create ternary operator:
+// //     if 18 >= 21 print "Adult"
+// //     else print "Minor"
+// let ag=18
+// if(ag>=21)
+// {console.log("major")}
+// else{console.log("minor")}
+
+// // 34. Create one array with 5 city names and print third city.
+
+// let citi=["Tirupur","Erode","Coimbatore","Salem","Chennai","Banglore"]
+// console.log(citi[2])
+
+// // 35. Create one object with employee details and print employee name.
+
+// let employee={name:"sathish",code:12345,domain:"software"}
+// console.log(employee.name)
+
+// // 36. Create variable with string "100" and print typeof.
+
+// let str="100"
+// console.log(typeof(str))//srting
+
+// // 37. Create variable with number 100 and print typeof.
+
+// let number=200
+// console.log(typeof(number))
+
+// // 38. Create one null variable and print it.
+
+// let vari=" "
+// console.log(vari)
+
+// // 39. Create one array with mixed datatypes.
+
+// let types=["cold",180,true," "]
+// console.log(types)
+
+// // 40. Create object with student details:
+// //     name, mark, passed(boolean)
+// let details={
+// name:"sathish",
+// mark:500,
+// passed:true
+// }
+// console.log(details)
+
+Task:3
+
+//1. Student Login Check
+//Ask username and password using prompt().
+//If username = "admin" and password = "1234"
+//→ print "Login Success"
+//Else print "Invalid User"
+
+let username = prompt("Enter username:");
+let password = prompt("Enter password:");
+
+if (username === "admin" && password === "1234") {console.log("Login Success");} else { console.log("Invalid User");}
+
+//2. ATM Balance System
+//Create a variable balance = 5000
+//Withdraw amount using prompt()
+//If amount less than balance → "Withdraw Success"
+//Else → "Insufficient Balance"
+
+let balance = 5000;
+let amount = Number(prompt("Enter withdraw amount:"));
+
+if (amount < balance) {console.log("Withdraw Success");} 
+else {console.log("Insufficient Balance");}
+
+
+//3. Traffic Signal Project
+//Using switch
+//"red" → stop
+//"yellow" → ready
+//"green" → go
+
+let signal = prompt("Enter signal color:");
+switch(signal) {
+    case "red":
+        console.log("Stop");break;
+
+    case "yellow":console.log("Ready");break;
+
+    case "green":console.log("Go");break;
+
+    default:console.log("Invalid Signal");}
+
+
+//4. Age Eligibility Checker
+//Take age from user.
+//age >= 18 → "Eligible for Vote"
+//Else → "Not Eligible"
+
+let age = Number(prompt("Enter age:"));
+if (age >= 18) 
+    {console.log("Eligible for Vote");}
+else{console.log("Not Eligible");}
+
+
+//5. Shopping Discount System
+//Take product price.
+//Above 5000 → 20% discount
+//Above 3000 → 10% discount
+//Else → no discount
+
+let price = Number(prompt("Enter product price:"));
+
+if (price > 5000) 
+    {console.log("20% Discount");} 
+else if (price > 3000) 
+    {console.log("10% Discount");}
+ else {console.log("No Discount");}
+
+//6. Password Strength Checker
+//Check password length.
+//length >= 8 → "Strong Password"
+//Else → "Weak Password"
+
+let pass = prompt("Enter password:");
+
+if (pass.length >= 8) 
+    {console.log("Strong Password");}
+ else 
+    {console.log("Weak Password");}
+
+//7. Mobile Battery Warning
+//Take battery percentage.
+//<= 10 → "Battery Low"
+//<= 50 → "Battery Medium"
+//Else → "Battery Full"
+
+let battery = Number(prompt("Enter battery percentage:"));
+
+if (battery <= 10) 
+    {console.log("Battery Low");} 
+else if (battery <= 50) 
+    {console.log("Battery Medium");} 
+else 
+    {console.log("Battery Full");}
+
+//8. Employee Salary Bonus
+//Create salary variable.
+//salary >= 50000 → 15% bonus
+//salary >= 30000 → 10% bonus
+//Else → 5% bonus
+
+let salary = Number(prompt("Enter salary:"));
+
+if (salary >= 50000) 
+    {console.log("15% Bonus");} 
+else if (salary >= 30000) 
+    {console.log("10% Bonus");} 
+else 
+    {console.log("5% Bonus");}
+
+//9. Online Food Order
+//Take order amount.
+//Above 499 → "Free Delivery"
+//Else → "Delivery Charge Added"
+
+let orderAmount = Number(prompt("Enter order amount:"));
+
+if (orderAmount > 499) 
+    {console.log("Free Delivery");} 
+else 
+    {console.log("Delivery Charge Added");}
+
+//10. Movie Ticket Booking
+//Take age.
+//Below 5 → Free ticket
+//Below 18 → Child ticket
+//Else → Adult ticket
+
+let movieAge = Number(prompt("Enter age:"));
+
+if (movieAge < 5) 
+    {console.log("Free Ticket");} 
+else if (movieAge < 18) 
+    {console.log("Child Ticket");} 
+else 
+    {console.log("Adult Ticket");}
+
+
+//11. Instagram Likes Counter
+//Using for loop
+//Print:
+//Like 1
+//Like 2
+//Like 3
+//Like 4
+//Like 5
+
+for (let i = 1; i <= 5; i++) 
+    {console.log("Like " + i);}
+
+//12. Countdown Timer
+//Using while loop
+//Print:
+//5
+//4
+//3
+//2
+//1
+//Start
+
+let count = 5;
+
+while (count >= 1) 
+    {console.log(count);
+    count--;}
+console.log("Start");
+
+//13. OTP Verification
+//Create OTP = 1234
+//Take input from user.
+//Correct → "OTP Verified"
+//Wrong → "Invalid OTP"
+
+let otp = 1234;
+let userOtp = Number(prompt("Enter OTP:"));
+
+if (userOtp === otp) 
+    {console.log("OTP Verified");} 
+else 
+    {console.log("Invalid OTP");}
+
+//14. E-commerce Cart Total
+//Array:
+//let cart = [200,500,1000,300]
+//Find total cart value using loop.
+
+let cart = [200, 500, 1000, 300];
+let total = 0;
+
+for (let item of cart)
+    {total += item;}
+console.log("Total Cart Value:", total);
+
+//15. Student Mark Grade System
+//Take mark.
+//90+ → A Grade
+//70+ → B Grade
+//50+ → C Grade
+//Else → Fail
+
+let mark = Number(prompt("Enter mark:"));
+if (mark >= 90)
+    {console.log("A Grade");} 
+else if (mark >= 70) 
+    {console.log("B Grade");} 
+else if (mark >= 50) 
+    {console.log("C Grade");} 
+else 
+    {console.log("Fail");}
+
+//16. Netflix Subscription Checker
+//Boolean variable:
+//let subscription = true
+//true → "Watch Movie"
+//false → "Buy Subscription"
+
+let subscription = true;
+if (subscription) 
+    {console.log("Watch Movie");} 
+else 
+    {console.log("Buy Subscription");}
+
+//17. Product Stock Checker
+//let stock = 0
+//stock > 0 → "Product Available"
+//Else → "Out of Stock"
+
+let stock = 0;
+if (stock > 0) 
+    {console.log("Product Available");} 
+else 
+    {console.log("Out of Stock");}
+
+//18. Weather App Condition
+//Using switch
+//sunny" → "Go Outside"
+//"rainy" → "Take Umbrella"
+//"cold" → "Wear Jacket"
+
+let weather = prompt("Enter weather:");
+switch(weather) 
+{
+case "sunny":
+        console.log("Go Outside");
+        break;
+
+    case "rainy":
+        console.log("Take Umbrella");
+        break;
+case "cold":
+        console.log("Wear Jacket");
+        break;
+default:
+        console.log("Invalid Weather");
 }
-console.log(bike)
 
-// 9. Print bike color from object.
+//19. User Profile Object
+//Create object:
+//let user = {
+   //name : "Navi",
+   //age : 20,
+   //city : "Bangalore"
+//}
+//Print all values using for in loop.
 
-console.log(bike.color)
+let user = 
+{
+name: "Navi",
+    age: 20,
+ city: "Bangalore"
+};
+for (let key in user) 
+    {console.log(user[key]);}
 
-// 10. Create two variables and subtract values.
+//20. WhatsApp Chat Array
+//Array:
+//let chats = ["hi","hello","where are you","ok"]
+//Print each message using for of loop.
 
-let q=10
-    w=5
-let sub=q-w
-console.log(sub)
-
-// 11. Create two variables and divide values.
-
-let r=10
-    t=2
-let div=r/t
-console.log(div)
-
-// 12. Find 15 % 2.
-let find=15%2
-console.log(find)
-OUTPUT:1
-
-// 13. Find 2 ** 5.
-
-let fin=2**5
-console.log(fin)
-OUTPUT:32
-
-// 14. Create variable with 5 and use post increment.
-
-let a=5;
-    a++
-console.log(a)
-
-// 15. Create variable with 10 and use pre increment.
-
-let no=10;
-    ++no;
-console.log(no)
-
-// 16. Create variable with 20 and use post decrement.
-
-let de=11;
-    de--;
-console.log(de)
-
-// 17. Create variable with 50 and use pre decrement.
-
-let va=20;
-    --va;
-console.log(va)
-
-// 18. Check 100 > 50.
-
-console.log(100>50) //true
-
-// 19. Check 25 < 10.
-
-console.log(25 < 10)//false
-
-// 20. Check 50 >= 50.
-
-console.log(50 >= 50)//true
-
-// 21. Check 30 <= 25.
-
-console.log(30 <= 25)//false
-
-// 22. Check 10 == "10".
-
-console.log(10 == "10")//true
-
-// 23. Check 10 === "10".
-
-console.log(10 === "10")//false
-
-// 24. Check 25 != "25".
-
-console.log(25 != "25")//false
-
-// 25. Check 25 !== "25".
-
-console.log(25 !== "25")//true
-
-// 26. Use AND operator:
-//     10 > 5 && 20 > 15
-
-console.log(10 > 5 && 20 > 15)//true
-
-// 27. Use AND operator:
-//     5 > 10 && 20 > 15
-
-console.log(5 > 10 && 20 > 15)//false
-
-// 28. Use OR operator:
-//     5 > 10 || 20 > 15
-
-console.log(5 > 10 || 20 > 15)//true
-
-// 29. Use OR operator:
-//     2 > 5 || 1 > 10
-
-console.log(2 > 5 || 1 > 10)//false
-
-// 30. Use NOT operator:
-//     !(10 > 5)
-
-console.log(!(10 > 5))//false
-
-// 31. Use NOT operator:
-//     !(5 > 10)
-
-console.log(!(5 > 10))//true
-
-// 32. Create ternary operator:
-//     if 50 > 25 print "True"
-//     else print "False"
-
-let l = 50 > 25;
-
-if(l){console.log(true);}
-else{console.log(false);}
-// 33. Create ternary operator:
-//     if 18 >= 21 print "Adult"
-//     else print "Minor"
-let ag=18
-if(ag>=21)
-{console.log("major")}
-else{console.log("minor")}
-
-// 34. Create one array with 5 city names and print third city.
-
-let citi=["Tirupur","Erode","Coimbatore","Salem","Chennai","Banglore"]
-console.log(citi[2])
-
-// 35. Create one object with employee details and print employee name.
-
-let employee={name:"sathish",code:12345,domain:"software"}
-console.log(employee.name)
-
-// 36. Create variable with string "100" and print typeof.
-
-let str="100"
-console.log(typeof(str))//srting
-
-// 37. Create variable with number 100 and print typeof.
-
-let number=200
-console.log(typeof(number))
-
-// 38. Create one null variable and print it.
-
-let vari=" "
-console.log(vari)
-
-// 39. Create one array with mixed datatypes.
-
-let types=["cold",180,true," "]
-console.log(types)
-
-// 40. Create object with student details:
-//     name, mark, passed(boolean)
-let details={
-name:"sathish",
-mark:500,
-passed:true
-}
-console.log(details)
+let chats = ["hi", "hello", "where are you", "ok"];
+for(let message of chats) 
+{console.log(message);}
